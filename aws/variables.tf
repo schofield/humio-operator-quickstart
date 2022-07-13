51f566cbf8b813e0d3f23c9513bf3ab3cae25af7
Grant Schofield <grant.schofield@crowdstrike.com>
2022-07-13 16:42:52 -0700

    Example humio cluster spec and AWS TF config for 100TB cluster

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,31 +1,58 @@
+# example changes for 100TB/day scale
+# use for reference
+
 variable "region" {
   type    = string
   default = "us-west-2"
 }
+
 variable "humio_instance_type" {
   type    = string
-  default = "i3.large" # Instance should have local NVME
+  default = "i3.8xlarge" # Instance should have local NVME
 }
+
+# Note at 100TB/day rate will only allow 15 days nvme storage on this node type
+# Assuming 10x compression
 variable "humio_instance_count" {
   type    = number
-  default = 3
+  default = 51
 }
+
+# using i3en.24xlarge
+# variable "humio_instance_type" {
+#   type    = string
+#   default = "i3en.24xlarge" # Instance should have local NVME
+# }
+
+# Note at 100TB/day rate will allows 52 days nvme storage on this node type
+# Assuming 10x compression
+# variable "humio_instance_count" {
+#   type    = number
+#   default = 18 
+# }
+
+
 variable "kafka_instance_type" {
   type    = string
-  default = "kafka.m5.large"
+  default = "kafka.m5.2xlarge"
 }
+
 variable "kafka_instance_count" {
   type    = number
-  default = 3
+  default = 12
 }
+
 variable "kafka_version" {
   type    = string
   default = "2.8.1"
 }
+
+# sized to keep 24 hours of data if needed
 variable "kafka_volume_size" {
   type    = number
-  default = 100
+  default = 5000
 }
+
 variable "humio_data_dir" {
   type    = string
   default = "/mnt/disks/vol1"
